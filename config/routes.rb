96aa129119371adfb7f56e3fe8441c0ebe353189
Arslan Khalid <arslankhalid067@gmail.com>
2020-03-19 15:58:17 +0500

    Product Categories Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products, only: %i[index show]
+  resources :categories, only: %i[show]
+  root 'products#index'
 end
